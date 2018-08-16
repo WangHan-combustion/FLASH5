34ceec85cbc8dc829aa5a43983a8e633ba02653e
Saurabh Chawdhary <saurabh.chawdhary@gmail.com>
2018-08-16 12:03:56 -0500

    Remove initialization of rhs, asol from unitTest which is now done in Simulation_initBlock and some more cleanup.

--- FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex_abeclap/Grid_unitTest.F90
+++ FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex_abeclap/Grid_unitTest.F90
@@ -167,42 +167,12 @@ use amrex_lo_bctypes_module
 !!init_prob_abeclap
     ascalar=0.
     bscalar=-1.
-    do ilev = 0, size(rhs)-1
-       !$omp parallel private(rlo,rhi,elo,ehi,alo,ahi,blo,bhi,bx,mfi,prhs,pexact,pa,pb)
-       call amrex_mfiter_build(mfi, rhs(ilev), tiling=.true.)
 
-       do while (mfi%next())
-          bx = mfi%tilebox()
-          gbx = mfi%growntilebox(1)
-          prhs   =>            rhs(ilev) % dataptr(mfi)
-          pexact => exact_solution(ilev) % dataptr(mfi)
-          pa     =>          acoef(ilev) % dataptr(mfi)
-          pb     =>          bcoef(ilev) % dataptr(mfi)
-          rlo = lbound(prhs)
-          rhi = ubound(prhs)
-          elo = lbound(pexact)
-          ehi = ubound(pexact)
-          alo = lbound(pa)
-          ahi = ubound(pa)
-          blo = lbound(pb)
-          bhi = ubound(pb)
-          call actual_init_abeclapcian(bx%lo, bx%hi, gbx%lo, gbx%hi, &
-               prhs, rlo(1:3), rhi(1:3), pexact, elo(1:3), ehi(1:3), &
-               pa, alo(1:3), ahi(1:3), pb, blo(1:3), bhi(1:3), &
-               amrex_problo, amrex_probhi, geom(ilev)%dx)
-       end do
-
-       call amrex_mfiter_destroy(mfi)
-       !$omp end parallel
-
-       ! This will be used to provide bc and initial guess for the solver.
-       call solution(ilev)%setVal(0.0_amrex_real)
-    end do
-
-!! solve abeclaplacian
     ! For ABecLaplacian, the b coefficents are on faces
     allocate(beta(amrex_spacedim,0:max_level))
     do ilev = 0, max_level
+      ! This will be used to provide bc and initial guess for the solver.
+       call solution(ilev)%setVal(0.0_amrex_real)
        do idim = 1, amrex_spacedim
           nodal = .false.
           nodal(idim) = .true.
@@ -214,6 +184,7 @@ use amrex_lo_bctypes_module
 !       call amrex_multifab_build_alias(beta(3,ilev), facevarz(ilev), BETA_FACE_VAR, 1)      
     end do
      
+!! solve abeclaplacian
        call amrex_abeclaplacian_build(abeclap, geom, ba, dm, &
             metric_term=.false., agglomeration=agglomeration, consolidation=consolidation, &
             max_coarsening_level=max_coarsening_level)
@@ -402,93 +373,6 @@ use amrex_lo_bctypes_module
      write(*,*) ' ' 
   endif
   return
-
-
-contains
-  subroutine actual_init_abeclapcian (lo, hi, glo, ghi, rhs, rlo, rhi, exact, elo, ehi, &
-       alpha, alo, ahi, beta, blo, bhi, prob_lo, prob_hi, dx)
-    integer, dimension(3), intent(in) :: lo, hi, glo, ghi, rlo, rhi, elo, ehi, alo, ahi, blo, bhi
-    real(amrex_real), intent(inout) :: rhs  (rlo(1):rhi(1),rlo(2):rhi(2),rlo(3):rhi(3))
-    real(amrex_real), intent(inout) :: exact(elo(1):ehi(1),elo(2):ehi(2),elo(3):ehi(3))
-    real(amrex_real), intent(inout) :: alpha(alo(1):ahi(1),alo(2):ahi(2),alo(3):ahi(3))
-    real(amrex_real), intent(inout) :: beta (blo(1):bhi(1),blo(2):bhi(2),blo(3):bhi(3))
-    real(amrex_real), dimension(3), intent(in) :: prob_lo, prob_hi, dx
-
-    integer :: i,j,k
-    real(amrex_real) x, y, z, xc, yc, zc
-    real(amrex_real) r, theta, dbdrfac
-    real(amrex_real) pi, fpi, tpi, fac
-    real(amrex_real), parameter :: w = 0.05d0
-    real(amrex_real), parameter :: sigma = 10.d0
-    integer :: bc_type=1
-    type(amrex_parmparse) pp
-
-    call amrex_parmparse_build(pp)
-    call pp % query("bc_type", bc_type)
-    call amrex_parmparse_destroy(pp)
-
-    pi = 4.d0 * atan(1.d0)
-    tpi = 2.0d0 * pi
-    fpi = 4.0d0 * pi
-    fac = 12.d0 * pi**2
-
-    xc = (prob_hi(1) + prob_lo(1))/2.d0
-    yc = (prob_hi(2) + prob_lo(2))/2.d0
-    zc = (prob_hi(3) + prob_lo(3))/2.d0
-
-    theta = 0.5d0*log(3.d0) / (w + 1.d-50)
-
-    do k = glo(3), ghi(3)
-       z = prob_lo(3) + dx(3) * (dble(k)+0.5d0)
-       do j = glo(2), ghi(2)
-          y = prob_lo(2) + dx(2) * (dble(j)+0.5d0)
-          do i = glo(1), ghi(1)
-             x = prob_lo(1) + dx(1) * (dble(i)+0.5d0)
-
-             r = sqrt((x-xc)**2 + (y-yc)**2 + (z-zc)**2)
-
-             beta(i,j,k) = 1.0 !(sigma-1.d0)/2.d0*tanh(theta*(r-0.25d0)) + (sigma+1.d0)/2.d0
-          end do
-       end do
-    end do
-
-    do k = lo(3), hi(3)
-       z = prob_lo(3) + dx(3) * (dble(k)+0.5d0)
-       do j = lo(2), hi(2)
-          y = prob_lo(2) + dx(2) * (dble(j)+0.5d0)
-          do i = lo(1), hi(1)
-             x = prob_lo(1) + dx(1) * (dble(i)+0.5d0)
-             r = sqrt((x-xc)**2 + (y-yc)**2 + (z-zc)**2)
-
-             dbdrfac = (sigma-1.d0)/2.d0/(cosh(theta*(r-0.25d0)))**2 * theta/r
-             dbdrfac = dbdrfac * bscalar
-
-             alpha(i,j,k) = 0. !1.d0
-
-!             exact(i,j,k) = 1.d0 * cos(tpi*x) * cos(tpi*y) * cos(tpi*z)   &
-!                  &      + .25d0 * cos(fpi*x) * cos(fpi*y) * cos(fpi*z)
-!
-!             rhs(i,j,k) = beta(i,j,k)*bscalar*fac*(cos(tpi*x) * cos(tpi*y) * cos(tpi*z)   &
-!                  &                        + cos(fpi*x) * cos(fpi*y) * cos(fpi*z))  &
-!                  &   + dbdrfac*((x-xc)*(tpi*sin(tpi*x) * cos(tpi*y) * cos(tpi*z)   &
-!                  &                     + pi*sin(fpi*x) * cos(fpi*y) * cos(fpi*z))  &
-!                  &            + (y-yc)*(tpi*cos(tpi*x) * sin(tpi*y) * cos(tpi*z)   &
-!                  &                     + pi*cos(fpi*x) * sin(fpi*y) * cos(fpi*z))  &
-!                  &            + (z-zc)*(tpi*cos(tpi*x) * cos(tpi*y) * sin(tpi*z)   &
-!                  &                     + pi*cos(fpi*x) * cos(fpi*y) * sin(fpi*z))) &
-!                  &                   + ascalar * (cos(tpi*x) * cos(tpi*y) * cos(tpi*z)   &
-!                  &               + 0.25d0 * cos(fpi*x) * cos(fpi*y) * cos(fpi*z))
-!!!!!!!!!!!!!!
-              exact(i,j,k) = cos(tpi*x)*cos(tpi*y)*cos(tpi*z)
-              rhs(i,j,k)   = -3.0*tpi*tpi*exact(i,j,k)
-!!!!!!!!
-          end do
-       end do
-    end do
-
-  end subroutine actual_init_abeclapcian
-
-
 
 end subroutine Grid_unitTest
 
