80fc6de70494e4df3342e4631a05f4fa23ebb5a4
Saurabh Chawdhary <saurabh.chawdhary@gmail.com>
2018-08-16 16:28:43 -0500

    Solve by calling a separate function Grid_solveAbecLaplacian and cleanup rest of the code

--- FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex_abeclap/Grid_unitTest.F90
+++ FLASH4.4/source/Simulation/SimulationMain/unitTest/Multigrid_Amrex_abeclap/Grid_unitTest.F90
@@ -29,7 +29,7 @@ subroutine Grid_unitTest(fileUnit,perfect)
 
   use Grid_interface, ONLY : GRID_PDE_BND_PERIODIC, GRID_PDE_BND_DIRICHLET, GRID_PDE_BND_NEUMANN,&
 !       Grid_getBlkIndexLimits, &
-       Grid_solvePoisson, &
+       Grid_solvePoisson, Grid_solveAbecLaplacian, &
        Grid_getBlkPtr,Grid_releaseBlkPtr, &
        Grid_getDeltas, Grid_fillGuardCells, &
        Grid_getLeafIterator, Grid_releaseLeafIterator
@@ -37,23 +37,8 @@ subroutine Grid_unitTest(fileUnit,perfect)
   use Grid_data, ONLY : gr_meshMe, gr_meshComm
   use leaf_iterator, ONLY : leaf_iterator_t
   use block_metadata, ONLY : block_metadata_t
-use amrex_amr_module
-use amrex_multifab_module
-use amrex_boxarray_module
-use amrex_distromap_module
-use amrex_geometry_module
-use amrex_fort_module
-use amrex_multigrid_module
-use amrex_abeclaplacian_module
-use amrex_lo_bctypes_module
-  use gr_amrexLsData, ONLY : gr_amrexLs_agglomeration, gr_amrexLs_consolidation, &
-                                    gr_amrexLs_linop_maxorder, gr_amrexLs_verbose, gr_amrexLs_cg_verbose, &
-                                    gr_amrexLs_max_iter,gr_amrexLs_max_fmg_iter,&
-                             gr_amrexLs_composite_solve, gr_amrexLs_ref_ratio
-  use gr_physicalMultifabs,      ONLY : unk!, facevarx, facevary, facevarz
- 
-!,     ONLY : amrex_init_from_scratch, &
-!                                   amrex_max_level
+use amrex_amr_module,     ONLY : amrex_init_from_scratch, &
+                                   amrex_max_level
 !  use gr_amrexLsInterface, ONLY : Grid_amrexLsSolvePoissonUnk
 
 #include "Flash.h"
@@ -92,135 +77,16 @@ use amrex_lo_bctypes_module
 
   integer TA(2),count_rate,ierr
   real :: ET
-!!example test vars..!!
- integer, save :: max_level = 1
-  integer, save :: ref_ratio = 2
-  integer, save :: n_cell = 32 !128
-  integer, save :: max_grid_size = 64
-  integer, save :: prob_type = 2
-  integer, save :: verbose = 2
-  integer, save :: cg_verbose = 0
-  integer, save :: max_iter = 100
-  integer, save :: max_fmg_iter = 0
-  integer, save :: bottom_solver = amrex_bottom_default
-  integer, save :: linop_maxorder = 2
-  logical, save :: agglomeration = .true.
-  logical, save :: consolidation = .true.
-  integer, save :: max_coarsening_level = 30
-
-  ! data
-  type(amrex_geometry), allocatable, save :: geom(:)
-  type(amrex_boxarray), allocatable, save :: ba(:)
-  type(amrex_distromap), allocatable, save :: dm(:)
-  type(amrex_multifab), allocatable, save :: solution(:)
-  type(amrex_multifab), allocatable, save :: rhs(:)
-  type(amrex_multifab), allocatable, save :: exact_solution(:)
-  type(amrex_multifab), allocatable, save :: acoef(:)
-  type(amrex_multifab), allocatable, save :: bcoef(:)
-  real(amrex_real), save :: ascalar, bscalar
-
-    integer ilev,idim
-    type(amrex_box) :: domain
-    type(amrex_box) :: dom
-
-    integer :: rlo(4), rhi(4), elo(4), ehi(4), alo(4), ahi(4), blo(4), bhi(4)
-    type(amrex_box) :: bx, gbx
-    type(amrex_mfiter) :: mfi
-    real(amrex_real), contiguous, dimension(:,:,:,:), pointer :: prhs, pexact, pa, pb
-
-    type(amrex_abeclaplacian) :: abeclap
-    type(amrex_multigrid) :: multigrid
-    integer(c_long) :: npts
-    real(amrex_real) :: err, avg1, avg2, offset
-    type(amrex_multifab), allocatable :: beta(:,:)
-    logical :: nodal(3)
-    type(amrex_multifab) :: null
+  real :: ascalar=0., bscalar=-1.
 
   ! -------------------------------------------------------------------
   bcTypes(:)=GRID_PDE_BND_NEUMANN  
-  bcTypes(3:4)=GRID_PDE_BND_DIRICHLET
   bcValues(:,:)=0.
-!!-----------------------------------------------------------!!
-!!allocate
-    max_level = amrex_get_finest_level()
-    allocate(geom(0:max_level))
-    allocate(ba(0:max_level))
-    allocate(dm(0:max_level))
-    allocate(solution(0:max_level))
-    allocate(rhs(0:max_level))
-    allocate(exact_solution(0:max_level))
-       allocate(acoef(0:max_level))
-       allocate(bcoef(0:max_level))
-
-!!init mf
-    do ilev = 0, max_level
-       call amrex_multifab_build_alias(solution(ilev), unk(ilev), NSOL_VAR, 1)
-       call amrex_multifab_build_alias(rhs(ilev), unk(ilev), RHS_VAR, 1)
-       call amrex_multifab_build_alias(exact_solution(ilev), unk(ilev), ASOL_VAR, 1)
-       call amrex_multifab_build_alias(acoef(ilev), unk(ilev), ALPHACC_VAR, 1)
-       call amrex_multifab_build_alias(bcoef(ilev), unk(ilev), BETACC_VAR, 1)
-       ba(ilev) = rhs(ilev)%ba
-       dm(ilev) = rhs(ilev)%dm
-       geom(ilev) = amrex_geom(ilev)
-    end do
-
-!!init_prob_abeclap
-    ascalar=0.
-    bscalar=-1.
-
-    ! For ABecLaplacian, the b coefficents are on faces
-    allocate(beta(amrex_spacedim,0:max_level))
-    do ilev = 0, max_level
-      ! This will be used to provide bc and initial guess for the solver.
-       call solution(ilev)%setVal(0.0_amrex_real)
-       do idim = 1, amrex_spacedim
-          nodal = .false.
-          nodal(idim) = .true.
-          call amrex_multifab_build(beta(idim,ilev), ba(ilev), dm(ilev), 1, 0, nodal)
-       end do
-       call amrex_average_cellcenter_to_face(beta(:,ilev), bcoef(ilev), geom(ilev))
-!       call amrex_multifab_build_alias(beta(1,ilev), facevarx(ilev), BETA_FACE_VAR, 1)
-!       call amrex_multifab_build_alias(beta(2,ilev), facevary(ilev), BETA_FACE_VAR, 1)
-!       call amrex_multifab_build_alias(beta(3,ilev), facevarz(ilev), BETA_FACE_VAR, 1)      
-    end do
-     
-!! solve abeclaplacian
-       call amrex_abeclaplacian_build(abeclap, geom, ba, dm, &
-            metric_term=.false., agglomeration=agglomeration, consolidation=consolidation, &
-            max_coarsening_level=max_coarsening_level)
-
-       call abeclap % set_maxorder(linop_maxorder)
-
-       ! This is set up to have homogeneous Neumann BC
-       call abeclap % set_domain_bc([amrex_lo_neumann, amrex_lo_neumann, amrex_lo_neumann], &
-            &                       [amrex_lo_neumann, amrex_lo_neumann, amrex_lo_neumann])
-
-       if (ilev > 0) then
-          ! use coarse level data to set up bc at corase/fine boundary
-          call abeclap % set_coarse_fine_bc(solution(ilev-1), ref_ratio)
-       end if
-
-       do ilev = 0, max_level
-          ! for problem with pure homogeneous Neumann BC, we could pass an empty multifab
-          call abeclap % set_level_bc(ilev, solution (ilev))
-       end do
-
-       call abeclap % set_scalars(ascalar, bscalar)
-       do ilev = 0, max_level
-          call abeclap % set_acoeffs(ilev, acoef(ilev))
-          call abeclap % set_bcoeffs(ilev, beta(:,ilev))
-       end do
-
-       call amrex_multigrid_build(multigrid, abeclap)
-       call multigrid % set_verbose(verbose)
-       call multigrid % set_cg_verbose(cg_verbose)
-       call multigrid % set_max_iter(max_iter)
-       call multigrid % set_max_fmg_iter(max_fmg_iter)
-       call multigrid % set_bottom_solver(bottom_solver)
 
   call mpi_barrier(gr_meshComm,ierr)
   if (gr_meshMe .eq. 0) CALL SYSTEM_CLOCK(TA(1),count_rate)  
-       err = multigrid % solve(solution, rhs, 1.e-10_amrex_real, 0.0_amrex_real)
+  poisfact=1.
+  call Grid_solveAbecLaplacian(NSOL_VAR, RHS_VAR, ALPHACC_VAR, BETACC_VAR, bcTypes, bcValues, poisfact,ascalar,bscalar)
   call mpi_barrier(gr_meshComm,ierr)
   if (gr_meshMe .eq. 0) then
      CALL SYSTEM_CLOCK(TA(2),count_rate)
@@ -228,24 +94,9 @@ use amrex_lo_bctypes_module
      write(*,*) ' ' 
      write(*,*) '3 PERIODIC Poisson Solver time = ',ET,' sec.'
   endif
-       call amrex_abeclaplacian_destroy(abeclap)
-       call amrex_multigrid_destroy(multigrid)
 
-!!Destroy-----------------------------------------------------------!!
-    do ilev = 0, max_level
-       call amrex_geometry_destroy(geom(ilev))
-       call amrex_boxarray_destroy(ba(ilev))
-       call amrex_distromap_destroy(dm(ilev))
-       call amrex_multifab_destroy(solution(ilev))
-       call amrex_multifab_destroy(rhs(ilev))
-       call amrex_multifab_destroy(exact_solution(ilev))
-       if (allocated(acoef)) then
-          call amrex_multifab_destroy(acoef(ilev))
-          call amrex_multifab_destroy(bcoef(ilev))
-       end if
-    end do
 
-!!-----------------------------------------------------------!!
+
   ! Check error in the solution:
   L2_err = 0.
   blkpoints = 0.
