8836213667024c9b0468b0796880153d9632d3aa
akashdhruv <akashdhruv@gwmail.gwu.edu>
2018-09-19 01:23:36 -0400

    setup file changes +noio

--- FLASH4.4/ins.sh
+++ FLASH4.4/ins.sh
@@ -1,1 +1,1 @@
-./setup unitTest/INS_amrex -auto -3d -nxb=16 -nyb=16 -nzb=16 -index-reorder -unit=Grid/GridMain/AMR/Amrex +serialio -unit=IO/IOMain/hdf5/serial/AM -maxblocks=16 -site=colonialone.gwu.edu -objdir=INS_amrex
+./setup unitTest/INS_amrex -auto -3d -nxb=16 -nyb=16 -nzb=16 -index-reorder -unit=Grid/GridMain/AMR/Amrex +noio -maxblocks=16 -site=colonialone.gwu.edu -objdir=INS_amrex
