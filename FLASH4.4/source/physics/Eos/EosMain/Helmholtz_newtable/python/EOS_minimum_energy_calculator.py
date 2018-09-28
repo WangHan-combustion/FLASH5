7cd9b6889508b66d13c285895fe76a2ae30920b8
Alexander Grannan <agrannan@anl.gov>
2018-09-27 19:57:30 -0500

    updated minimum energy calculator python file

--- FLASH4.4/source/physics/Eos/EosMain/Helmholtz_newtable/python/EOS_minimum_energy_calculator.py
+++ FLASH4.4/source/physics/Eos/EosMain/Helmholtz_newtable/python/EOS_minimum_energy_calculator.py
@@ -44,42 +44,46 @@ asol    = 4.0 * ssol / clight
 
 
 
-def EOS_minimum_energy_calculator(den,abar,zbar):
+def EOS_minimum_energy_calculator(den,Ye,sumy):
     
     if type(den) is not np.ndarray:
         den = den*np.ones(1)
     
-    if type(abar) is not np.ndarray:
-        abar = abar*np.ones(1)
+    if type(sumy) is not np.ndarray:
+        sumy = sumy*np.ones(1)
         
-    if type(zbar) is not np.ndarray:
-        zbar = zbar*np.ones(1)    
+    if type(Ye) is not np.ndarray:
+        Ye = Ye*np.ones(1)    
     
     
-    e_min = np.ones((len(den),len(abar),len(zbar)))
-    
-    
+    e_min = np.ones((len(den),len(Ye),len(sumy)))
+     
     for j in range(0,len(den)):
-        for k in range(0,len(abar)):
-            for l in range(0,len(zbar)):
+        for k in range(0,len(Ye)):
+            for l in range(0,len(sumy)):
                 
                 num_density_0 = 8*pi*(me*clight/h)**3.0/3
             
-                num_density_ele = den[j]*avo*zbar[l]/abar[k]
+                num_density_ele = den[j]*avo*Ye[k]
             
-                fermi_momentum_norm = (num_density_ele/num_density_0)**(1.0/3.0)
+                x0 = (num_density_ele/num_density_0)**(1.0/3.0)
             
-                fermi_momentum = fermi_momentum_norm*me*clight
+                fermi_momentum = x0*me*clight
             
-                den_trans = 8*pi*(2.5*me*clight)**3.0/3/h**3/zbar[k]/abar[l]/avo
+                den_trans = 8*pi*(2.5*me*clight)**3.0/3/h**3/Ye[k]/avo
+                c1 = num_density_0*mecc
                 
-                if (fermi_momentum_norm >= 1.0):
-                    e_min[j,k,l] = num_density_0*mecc*(-8*fermi_momentum_norm**3 \
-                            + 3*sqrt(fermi_momentum_norm**2 + 1)*(2*fermi_momentum_norm**3 \
-                            + fermi_momentum_norm) - 3*log(fermi_momentum_norm \
-                            + sqrt(1 + fermi_momentum_norm**2)))/8/den[j]
-                else:
-                    e_min[j,k,l] = num_density_0*mecc*(3*fermi_momentum_norm**5/10)/den[j] 
+##                if (x0 >= 1.0):
+#                e_min_1[j,k,l] = c1*(-8*x0**3 \
+#                        + 3*sqrt(x0**2 + 1)*(2*x0**3 \
+#                        + x0) - 3*log(x0 \
+#                        + sqrt(1 + x0**2)))/8/den[j]
+#                else:
+#                    e_min_1[j,k,l] = num_density_0*mecc*(3*x0**5/10)/den[j] 
+                
+                c2 = pi*me**4.0*clight**5.0/3.0/h**3.0
+                e_min[j,k,l] = c2*(8.0*x0**3.0*(sqrt(x0**2.0 + 1.0) - 1.0) -\
+                (x0*(2.0*x0**2.0 - 3.0)*sqrt(x0**2.0 + 1.0) + 3.0*asinh(x0)))/den[j]
     
 # Analytical Solution   
 #    e_min = num_density_0*mecc*(-8*fermi_momentum_norm**3 \
@@ -90,24 +94,24 @@ def EOS_minimum_energy_calculator(den,abar,zbar):
     return (e_min)
 
 
-def EOS_minimum_energy_calculator_test(den,abar,zbar):
+def EOS_minimum_energy_calculator_test(den,Ye,sumy):
     
-    e_min = EOS_minimum_energy_calculator(den,abar,zbar)
+    e_min_1 = EOS_minimum_energy_calculator(den,Ye,sumy)
     
     return(e_min)
 abar_lo = 1.0
 abar_hi = 32.0
-abar_range = np.linspace(abar_lo, abar_hi)
+sumy_range = np.linspace(1.0/abar_hi, 1.0/abar_lo)
 
-zbar_lo = 1.0
-zbar_hi = 16.0
-zbar_range = np.linspace(zbar_lo, zbar_hi)
+Ye_lo = 0.1
+Ye_hi = 1.0
+Ye_range = np.linspace(Ye_lo,Ye_hi)
 
-dlo = log10(1.0e-12)
+dlo = log10(1.0e-15)
 dhi = log10(1.0e15)
 d_range=np.logspace(dlo, dhi)
 
-e_min = EOS_minimum_energy_calculator_test(d_range,abar_range,zbar_range)
+e_min = EOS_minimum_energy_calculator(d_range,Ye_range,sumy_range)
 
 #num_density_0 = 8*pi*(me*clight/h)**3.0/3
 #    
@@ -134,10 +138,12 @@ e_min = EOS_minimum_energy_calculator_test(d_range,abar_range,zbar_range)
 #         
 #plt.figure(1)
 ##plt.plot(d_range_log, e_min_1,label='w/ Rest Mass')
-#plt.plot(d_range, e_min[:,0,0],label='w/o Rest Mass')
+#plt.plot(d_range, e_min_1[:,-1,0],label='emin1')
+#plt.plot(d_range, e_min_2[:,-1,0],label='emin2')
 ##plt.plot(d_range, e_min_3,label='NR Limit')
 ##plt.plot(d_range, e_min_4,label='UR Limit')
 #plt.yscale('log')
 #plt.xscale('log')
 #plt.legend()
-#   
+#plt.grid()
+   
